17d24e21ff6c598f1cd4e48657bb3595d961c049
Simon Enström <simonenstrom@hotmail.com>
2018-09-30 19:23:23 +0200

    Added config file for sensitive information

--- VirkesbörsenAPI/REST_API/management/commands/refreshScrapeData.py
+++ VirkesbörsenAPI/REST_API/management/commands/refreshScrapeData.py
@@ -1,11 +1,12 @@
 from django.core.management.base import BaseCommand
 from ._scrapeData import WebScraper
 from REST_API.models import AuctionItem
+import config
 
 class Command(BaseCommand):
     help = 'Refresh scrape data in database'
     def handle(self, *args, **kwargs):
-        scraper = WebScraper('https://www.virkesborsen.se', '/auctions/', '/login/', 'username', 'password')
+        scraper = WebScraper('https://www.virkesborsen.se', '/auctions/', '/login/', config.EMAIL, config.PASSWORD)
         auctionDataItems = scraper.getAuctionItems()
         for auctionData in auctionDataItems:
             AuctionItem.objects.update_or_create(auctionData, auctionId=auctionData['auctionId'])
