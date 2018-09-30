70cbdf1d4db22b741f7a84847a1f3eb44c5cfa5e
Simon Enström <simonenstrom@hotmail.com>
2018-09-30 19:14:27 +0200

    removed sensitive data (don't worry password is changed)

--- VirkesbörsenAPI/REST_API/management/commands/refreshScrapeData.py
+++ VirkesbörsenAPI/REST_API/management/commands/refreshScrapeData.py
@@ -5,7 +5,7 @@ from REST_API.models import AuctionItem
 class Command(BaseCommand):
     help = 'Refresh scrape data in database'
     def handle(self, *args, **kwargs):
-        scraper = WebScraper('https://www.virkesborsen.se', '/auctions/', '/login/', 'simonenstrom@hotmail.com', 'Nm5gNrNTHPkogB2amHUA')
+        scraper = WebScraper('https://www.virkesborsen.se', '/auctions/', '/login/', 'username', 'password')
         auctionDataItems = scraper.getAuctionItems()
         for auctionData in auctionDataItems:
             AuctionItem.objects.update_or_create(auctionData, auctionId=auctionData['auctionId'])
